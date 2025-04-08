5a78af5c22cf4961f1172517959ccc7bc3d1ff84
Sourabh Jyoti Das <dassourabh126@gmail.com>
2025-04-08 23:53:31 +0530

    Ezzzzzzzzzzzzzzzz

--- src/app/api/videos/create/route.ts
+++ src/app/api/videos/create/route.ts
@@ -1,13 +1,35 @@
 import { connectToDatabase } from "@/app/lib/database";
 import Video from "../../../models/Video";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/options";
 
-export async function GET(params: type) {
+export async function POST(req: NextRequest) {
   try {
-    await connectToDatabase();
-    const videos = await Video.find({});
+    const session = await getServerSession(authOptions);
 
-    return NextResponse.json({ success: true, videos }, { status: 200 });
+    if (!session) {
+      return NextResponse.json(
+        { success: false, message: "Not loggedIn" },
+        { status: 401 }
+      );
+    }
+
+    await connectToDatabase();
+
+    const { title, description, videoUrl, thumbnailUrl } = await req.json();
+    if (!title || !description || !videoUrl || !thumbnailUrl) {
+      return NextResponse.json(
+        { success: false, message: "Fill all the feilds" },
+        { status: 400 }
+      );
+    }
+    await Video.create({title, description, videoUrl, thumbnailUrl});
+
+    return NextResponse.json(
+      { success: true, message: "Videos uploaded successfully" },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json(
       { success: false, message: error },
--- src/app/models/Video.ts
+++ src/app/models/Video.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 
 
 export const VIDEO_DIMENSIONS = {
-   height:1920
+   height:1920,
    width:1080,
 } as const
 
